ca78621011bfa50acb2775d2b3dbb37984da6dbc
Samer-Ismael <samerismaeelbat2@hotmail.com>
2023-11-07 12:29:54 +0100

    Saving

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+def generate_frames():
+    camera = cv2.VideoCapture(0)  # Use the default camera (usually your webcam)
+
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if not ret:
+                break
+
+            frame_bytes = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/camera')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
